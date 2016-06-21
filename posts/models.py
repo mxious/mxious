8e18d1dc66b9b425c02f6c5b9db01b86561371a8
Sergio <sergiomattei@outlook.com>
2016-06-21 19:13:13 -0400

    Created Posts application

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	user = models.ForeignKey(User)
+	title = models.CharField(max_length=50)
+	image = models.CharField(max_length=500)
+	content = models.CharField(max_length=255)
+	upvotes = models.IntegerField(default=0)
+	downvotes = models.IntegerField(default=0)
+	score = models.IntegerField(default=0)
+	published = models.DateTimeField('time published', auto_now_add=True)
