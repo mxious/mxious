7141e36642df5858a474a9c348a6db8f4bd9886e
Sergio Diaz <sergiomattei@outlook.com>
2016-06-21 00:26:40 -0400

    Fixed bug where gravatar() wouldn't work and increased image size
    Signed-off-by: Sergio Diaz <sergiomattei@outlook.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from hashlib import md5
 
 class Profile(models.Model):
 	user = models.OneToOneField(User)
@@ -8,7 +9,7 @@ class Profile(models.Model):
 	favorite_music = models.CharField(max_length=255)
 	follows = models.ManyToManyField('self', symmetrical=False)
 
-	def avatar(self, default='identicon', size='80', rating='pg'):
+	def avatar(self, default='identicon', size='150', rating='pg'):
 		email = self.user.email
 		email_hash = md5(email.encode('utf-8')).hexdigest()
 		url = "http://gravatar.com/avatar/%s?s=%s&d=%s&r=%s" % (email_hash, size, default, rating)
