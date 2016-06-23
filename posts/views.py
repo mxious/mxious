0be24ef07c34f2ca1b8566ea23d8bb51dd2682f5
Sergio <sergiomattei@outlook.com>
2016-06-23 04:29:54 -0400

    Use the ajax_required decorator and return bad_request on invalid input

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from mxious.decorators import ajax_required
 from .models import Post
 
 @login_required
@@ -13,36 +14,40 @@ def dashboard(request):
 	context = {'posts': posts, 'offset': settings.POST_DISPLAY_LIMIT}
 	return render(request, 'posts/dashboard.html', context)
 
-@csrf_exempt
+@ajax_required
 def load_more(request):
-	if request.is_ajax():
-		feed_type = request.POST.get('type')
+	try:
 		offset = int(request.POST.get('offset'))
-		if feed_type == 'following' and request.user.is_authenticated():
-			posts = Post.get_posts(user=request.user, offset=offset)
-			context = {'posts': posts}
-			json = {
-				'html': render_to_string('posts/raw_posts.html', context),
-				'count': posts.count(),
-				'success': True
-			}
-			return JsonResponse(json)
-	else:
-		return dashboard(request)
+	except ValueError:
+		return HttpResponseBadRequest()
 
-@csrf_exempt
+	feed_type = request.POST.get('type')
+
+	if feed_type == 'following' and request.user.is_authenticated():
+		posts = Post.get_posts(user=request.user, offset=offset)
+		context = {'posts': posts}
+		json = {
+			'html': render_to_string('posts/raw_posts.html', context),
+			'count': posts.count(),
+			'success': True
+		}
+		return JsonResponse(json)
+
+@ajax_required
 def poll(request):
-	if request.is_ajax():
+	try:
 		last_id = int(request.POST.get('last_id'))
-		feed_type = request.POST.get('feed_type')
-		if feed_type == 'following' and request.user.is_authenticated():
-			posts = Post.get_posts(user=request.user, after_id=last_id)
-			context = {'posts': posts}
-			json = {
-				'html': render_to_string('posts/raw_posts.html', context),
-				'count': posts.count(),
-				'success': True
-			}
-			return JsonResponse(json)
-	else:
-		return dashboard(request)
+	except ValueError:
+		return HttpResponseBadRequest()
+
+	feed_type = request.POST.get('feed_type')
+	
+	if feed_type == 'following' and request.user.is_authenticated():
+		posts = Post.get_posts(user=request.user, after_id=last_id)
+		context = {'posts': posts}
+		json = {
+			'html': render_to_string('posts/raw_posts.html', context),
+			'count': posts.count(),
+			'success': True
+		}
+		return JsonResponse(json)
