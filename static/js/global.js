a7ae17bf1073828742f937a71e843611d4129f55
Sergio <sergiomattei@outlook.com>
2016-06-23 04:30:08 -0400

    Created the global Mxious object

--- static/js/global.js
+++ static/js/global.js
@@ -2,11 +2,29 @@ $(function () {
     $("#menu").tooltip('show');
 });
 
-var grid = $('#grid');
-grid.imagesLoaded(function() {
-	grid.masonry({
-	    isFitWidth:true,
-	    isAnimated: true,
-	    columnWidth: '.card-std'
-	});
-});
+var Mxious = {
+	init: function (config) {
+		this.parameters = config;
+		this.csrf_config();
+	},
+	config: function (param) {
+		return this.parameters[param]
+	},
+
+	bind: function () {
+	},
+
+	csrf_config: function () {
+		function csrfSafeMethod(method) {
+		    // these HTTP methods do not require CSRF protection
+		    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+		}
+		$.ajaxSetup({
+		    beforeSend: function(xhr, settings) {
+		        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+		            xhr.setRequestHeader("X-CSRFToken", Mxious.config('csrf_token'));
+		        }
+		    }
+		});
+	}
+}
