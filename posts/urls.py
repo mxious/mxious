368aac2b2b0173a7901a6d0c8942f64aded44ada
Sergio Diaz <sergiomattei@outlook.com>
2016-06-25 23:51:44 -0400

    Added polling urls
    Signed-off-by: Sergio Diaz <sergiomattei@outlook.com>

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
+	url(r'^create', views.create, name='create'),
 	url(r'^load_more', views.load_more, name='load_more'),
-	url(r'^poll', views.poll, name='poll')
+	url(r'^poll', views.poll, name='poll'),
 ]
